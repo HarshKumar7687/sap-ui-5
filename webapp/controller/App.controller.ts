750d79546243849439455002305fb914a87307a1
Harsh <drharshkumar499@gmail.com>
2026-09-03 23:25:17 +0530

    model json

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,10 +1,22 @@
 import MessageToast from "sap/m/MessageToast";  //modules
 import Controller from "sap/ui/core/mvc/Controller"; //controller
+import JSONModel from "sap/ui/model/json/JSONModel"; //json model
 
 // /**
 //  * @name ui5.walkthrough.controller.App
 //  */
 export default class AppController extends Controller {
+	onInit(): void {
+	  // set data model on view
+		const data = {
+		   recipient: {
+			  name: "World"
+		   }
+		};
+		const dataModel = new JSONModel(data);
+		this.getView()?.setModel(dataModel);
+	}
+	
 	onShowHello(): void {
 		// alert("Hello World");
 		MessageToast.show("Hello World");
