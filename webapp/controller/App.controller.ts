1c6650c667f5b21eb8d1077d9a3ed3e578007a11
Harsh <drharshkumar499@gmail.com>
2026-09-04 23:06:57 +0530

    Components setup

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,32 +1,52 @@
-import MessageToast from "sap/m/MessageToast";  //modules
-import Controller from "sap/ui/core/mvc/Controller"; //controller
-import JSONModel from "sap/ui/model/json/JSONModel"; //json model
+// import MessageToast from "sap/m/MessageToast";  //modules
+// import Controller from "sap/ui/core/mvc/Controller"; //controller
+// import JSONModel from "sap/ui/model/json/JSONModel"; //json model
+// import ResourceModel from "sap/ui/model/resource/ResourceModel";
+// import ResourceBundle from "sap/base/i18n/ResourceBundle";
+
+// // /**
+// //  * @name ui5.walkthrough.controller.App
+// //  */
+// export default class AppController extends Controller {
+// 	onInit(): void {
+// 	  // set data model on view
+// 		const oData = {
+// 		   recipient: {
+// 			  name: "World"
+// 		   }
+// 		};
+// 		const oModel = new JSONModel(oData);
+// 		this.getView()?.setModel(oModel,"Recipient");
+// 		const i18nModel = new ResourceModel({
+// 				bundleName: "ui5.walkthrough.i18n.i18n"
+// 		});
+// 		this.getView()?.setModel(i18nModel, "i18n");
+// 	}
+
+// 	onShowHello(): void {
+// 		const recipient = (this.getView()?.getModel("Recipient") as JSONModel)?.getProperty("/recipient/name");
+// 		const resourceBundle = (this.getView()?.getModel("i18n") as ResourceModel)?.getResourceBundle() as ResourceBundle;
+// 		const msg = resourceBundle.getText("helloMsg", [recipient]) as string;
+// 		MessageToast.show(msg);
+// 	 }
+// };
+
+import MessageToast from "sap/m/MessageToast";
+import Controller from "sap/ui/core/mvc/Controller";
+import JSONModel from "sap/ui/model/json/JSONModel";
 import ResourceModel from "sap/ui/model/resource/ResourceModel";
 import ResourceBundle from "sap/base/i18n/ResourceBundle";
 
-// /**
-//  * @name ui5.walkthrough.controller.App
-//  */
+/**
+ * @name ui5.tutorial.walkthrough.controller.App
+ */
 export default class AppController extends Controller {
-	onInit(): void {
-	  // set data model on view
-		const oData = {
-		   recipient: {
-			  name: "World"
-		   }
-		};
-		const oModel = new JSONModel(oData);
-		this.getView()?.setModel(oModel,"Recipient");
-		const i18nModel = new ResourceModel({
-				bundleName: "ui5.walkthrough.i18n.i18n"
-		});
-		this.getView()?.setModel(i18nModel, "i18n");
-	}
-
 	onShowHello(): void {
+		// read msg from i18n model
 		const recipient = (this.getView()?.getModel("Recipient") as JSONModel)?.getProperty("/recipient/name");
 		const resourceBundle = (this.getView()?.getModel("i18n") as ResourceModel)?.getResourceBundle() as ResourceBundle;
 		const msg = resourceBundle.getText("helloMsg", [recipient]) as string;
+		// show message
 		MessageToast.show(msg);
-	 }
+	}
 };
--- webapp/index.ts
+++ webapp/index.ts
@@ -4,13 +4,24 @@
     ──────┬──────────       │        │
      namespace            folder   view name
 */
-import XMLView from "sap/ui/core/mvc/XMLView";
+// import XMLView from "sap/ui/core/mvc/XMLView";
 
-XMLView.create({
-	viewName: "ui5.walkthrough.view.App",
-	id: "app"
-}).then(function (oView) {
-	oView.placeAt("content");
-});
+// XMLView.create({
+// 	viewName: "ui5.walkthrough.view.App",
+// 	id: "app"
+// }).then(function (oView) {
+// 	oView.placeAt("content");
+// });
 
 
+import ComponentContainer from "sap/ui/core/ComponentContainer";
+
+new ComponentContainer({
+	id: "container",
+	name: "ui5.walkthrough",
+	settings: {
+		id: "walkthrough"
+	},
+	autoPrefixId: true,
+	async: true
+}).placeAt("content");
